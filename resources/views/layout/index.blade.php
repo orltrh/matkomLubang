7f1255ab8a82383f2a07717ddb46dbb4efa5dcc3
orltrh <aurelyataroreh@gmail.com>
2023-10-28 21:58:11 +0700

    first commit

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'SafeSpeaks')</title>
+    <!-- Favicons -->
+    <link href="{{ url('assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ url('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="{{ url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i') }}" rel="stylesheet">
+
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ url('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
+
+
+    @section('head')
+
+</head>
+@section('body')
+<body>
+
+  {{-- Content --}}
+  @section('content')
+  <div class="main">
+    <div class="container-fluid text-center" >
+        @show
+    </div>
+  </div>
+
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+
+        <div class="container">
+            <div class="copyright">
+            &copy; Copyright <strong><span>Putri Kecil Ayah</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+            Designed by <a href="#">Team Matkom Kelompok Putri Kecil Ayah</a>
+            </div>
+        </div>
+        </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ url('assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/php-email-form/validate.js') }}"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{ url('assets/js/main.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/map.blade.php
@@ -0,0 +1,102 @@
+@extends('layout.index')
+@section('title', 'Map')
+{{-- @section('menuTrack', 'active') --}}
+
+@section('head')
+    {{-- Template Routing Machine Leaflet --}}
+    <link rel="stylesheet" href="{{ url('assets/js/leaflet-routing-machine/dist/leaflet-routing-machine.css') }}" />
+
+    {{-- Template Leaflet Maps --}}
+    <link rel="stylesheet" href="{{ url('https://unpkg.com/leaflet@1.9.3/dist/leaflet.css') }}"
+    integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+    crossorigin=""/>
+
+    {{-- Template CSS Leaflet Maps --}}
+    <style>
+        #map { height: 600px; }
+
+        #map-container {
+            height: 610px;
+            width: 100%;
+            position: static;
+            overflow: hidden;
+            box-shadow: 0 8px 20px rgba(13, 80, 174, 0.2); /* Menambahkan bayangan */
+            background-clip: border-box;
+            border-radius: 10px;
+        }
+    </style>
+
+    {{-- Template Boostrap --}}
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+
+    {{-- Template Leaflet CSS JS --}}
+    <script src="{{ url('https://unpkg.com/leaflet@1.9.3/dist/leaflet.js') }}"
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+    crossorigin=""></script>
+
+    {{-- Template Routing Machine Leaflet CSS JS --}}
+    <script src="{{ url('assets/js/leaflet-routing-machine/dist/leaflet-routing-machine.js') }}"></script>
+
+    {{-- Template Geocoder --}}
+    <script src="{{ url('assets/js/leaflet-routing-machine/examples/Control.Geocoder.js') }}"></script>
+
+    {{-- Template Jquery --}}
+    <script src="{{ url('https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js') }}"></script>
+
+@section('content')
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-lg-3 col-md-5">
+                <form class="d-flex" role="search" id="searchForm">
+                    @csrf
+                    <input id="searchInput" class="form-control me-2" type="search" placeholder="Cari Username" name="username">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="container mt-3" id="map-container">
+        <div id="map"></div>
+    </div>
+
+
+    <script>
+        // Menampilkan maps
+        var map = L.map('map').setView([-7.9933885, 112.6079343], 15);
+
+        // // Menambahkan tile layer
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        // Mengaktifkan fitur geolocation
+        map.locate({setView: true, maxZoom:18});
+
+        // Menampilkan marker untuk menunjukkan lokasi user saat ini
+        var marker = L.marker([-7.9933885, 112.6079343]).addTo(map);
+        marker.bindPopup("<b>Lubang Berada Disini").openPopup();
+
+        // Fungsi untuk menambahkan titik pada polyline
+        function addLatLng(latlng) {
+            polyline.addLatLng(latlng);
+        }
+
+        var popup = L.popup();
+
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("Anda menekan map di titik " + e.latlng.toString())
+                .openOn(map);
+        }
+
+        map.on('click', onMapClick);
+    </script>
+
+</section>
+@endsection
+
